a207cc8f3b8e23dbd1f91c24e00376365289f852
Jensen Koch <jtkoch12@gmail.com>
2020-04-10 15:37:21 -0500

    Finished MVP

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express")
-
+const postsRouter = require("./routes/posts-router")
 const server = express()
 const port = 4000
 
 server.use(express.json())
+server.use("/api/posts", postsRouter)
+
+server.get("/", (req, res) => {
+  res.status(200).send("main route working");
+})
 
 server.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
